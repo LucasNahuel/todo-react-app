fa43aea8d5fa7f7062f3db948983f61b7cdd7b7b
LucasNahuel <munozlucas96@gmail.com>
2023-06-23 23:20:46 -0300

    react project initialized

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomHeader from './CustomHeader';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CustomHeader></CustomHeader>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CustomHeader.js
@@ -0,0 +1,13 @@
+
+function CustomHeader(props){
+
+    return(
+        <div className="header">
+            <h1>ToDo app</h1>
+        </div>
+    );
+
+}
+
+
+export default CustomHeader;
